fda3f37a145e8c0920931311a8f91a906d99593d
Andrey Alekseev <andrey.alekseev@saritasa.com>
2022-08-21 14:11:25 +0700

    Add skeleton loader #JC19-466

--- apps/react/src/features/anime/pages/AnimesPage/AnimesPage.tsx
+++ apps/react/src/features/anime/pages/AnimesPage/AnimesPage.tsx
@@ -1,8 +1,9 @@
 import { FC, memo, useEffect } from 'react';
 import { useAppDispatch, useAppSelector } from '@js-camp/react/store';
-import { selectAllAnimeBase } from '@js-camp/react/store/anime/selectors';
+import { selectAllAnimeBase, selectIsAnimeBaseLoading } from '@js-camp/react/store/anime/selectors';
 import { getAnimeList } from '@js-camp/react/store/anime/dispatchers';
 import { Outlet } from 'react-router-dom';
+import { Box } from '@mui/material';
 
 import { AnimeList } from '../../components/AnimeList';
 
@@ -11,17 +12,20 @@ import style from './AnimePage.module.css';
 const AnimesPageComponent: FC = () => {
   const dispatch = useAppDispatch();
   const animeList = useAppSelector(selectAllAnimeBase);
+  const isLoading = useAppSelector(selectIsAnimeBaseLoading);
+
   useEffect(() => {
     dispatch(getAnimeList());
   }, [dispatch]);
 
   return (
     <div className={style['anime-page-wrapper']}>
-      <AnimeList
-        className={style['anime-list']}
-        animeList={animeList}
-      ></AnimeList>
-      <Outlet/>
+      <Box className={style['anime-list']}>
+        <AnimeList animeList={animeList} isLoading={isLoading}/>
+      </Box>
+      <Box className={style['anime-details']}>
+        <Outlet/>
+      </Box>
     </div>
   );
 };
