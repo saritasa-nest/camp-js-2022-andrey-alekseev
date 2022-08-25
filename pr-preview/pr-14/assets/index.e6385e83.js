6866bbdb7eeaf745acfa9715167c0036fa5e584a
aletor123 <aletor123@users.noreply.github.com>
2022-08-25 10:22:48 +0000

    Deploy preview for PR 14 🛫

---
+++ pr-preview/pr-14/assets/index.e6385e83.js
@@ -0,0 +1,1 @@
+import{r as s,j as i,a as e,T as n}from"./index.abc2054f.js";const a=()=>i("div",{children:[e(n,{children:"Anime"}),e(n,{children:"Anime name"})]}),o=s.exports.memo(a);export{o as AnimeDetails};
