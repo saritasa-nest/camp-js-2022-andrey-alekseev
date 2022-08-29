54f308ec12cda8be2567c8a22bc84ddb0f9abcaf
dbitkovski <dbitkovski@users.noreply.github.com>
2022-08-29 08:00:16 +0000

    Remove preview for PR 14 🛬

--- pr-preview/pr-14/assets/index.e6385e83.js
+++
@@ -1,1 +0,0 @@
-import{r as s,j as i,a as e,T as n}from"./index.abc2054f.js";const a=()=>i("div",{children:[e(n,{children:"Anime"}),e(n,{children:"Anime name"})]}),o=s.exports.memo(a);export{o as AnimeDetails};
